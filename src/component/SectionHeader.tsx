2421ddfdd2cb3dfd1ff88b3dd84223ceae797b02
skywalker <houzhenhong@hotmail.com>
2019-09-22 16:06:35 +0800

    feat(section): header 展示第几关

--- src/component/SectionHeader.tsx
+++ src/component/SectionHeader.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-px2vw';
+import useRouter from 'use-react-router';
 import BaseBack from './BaseBack';
+import convertNumberToUppercase from '../utils/convertNumberToUppercase';
+import SectionTime from './SectionTime';
+import { ISectionProp } from '../interface/ISectionProp';
 
 const Header = styled.header`
   position: relative;
   margin: 40px 42px 0 42px;
   display: flex;
+  justify-content: center;
 `;
 
-const SectionHeader: React.FC = () => (
-  <Header>
-    <BaseBack />
-  </Header>
-);
+const Text = styled.div`
+  font-family: LeZhen, serif;
+  font-size: 36px;
+`;
+
+const SectionHeader: React.FC = () => {
+  const {
+    match: { params },
+  } = useRouter<ISectionProp>();
+  const { step } = params;
+  const [stepNum] = useState(convertNumberToUppercase(step));
+
+  return (
+    <>
+      <Header>
+        <BaseBack />
+        <Text>第{stepNum}关</Text>
+        <SectionTime isRunning />
+      </Header>
+    </>
+  );
+};
 
 export default SectionHeader;

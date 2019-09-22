36ce66632e2c18dc4a4a18aaa3cdd9818f2e6506
skywalker <houzhenhong@hotmail.com>
2019-09-22 10:41:45 +0800

    feat(all): 样式主题 & 修复路由动画

--- src/styled.ts
+++ src/styled.ts
@@ -1,8 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 import font from './assets/font/font.css';
 
+export const Theme = {
+  color: {
+    backgroundColor: '#ffe2ae',
+  },
+};
+export const PositionAll = {
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+};
+
 // eslint-disable-next-line import/prefer-default-export
 export const GlobalStyle = createGlobalStyle`
   // 引入字体
   ${font}
+
+  html, body {
+     position: absolute;
+     ${PositionAll};
+     overflow: hidden;
+  }
 `;

d868439c5523d9bcda788963dafabf62d5fc99ea
skywalker <houzhenhong@hotmail.com>
2019-09-23 17:10:50 +0800

    chore(section): 测试数据

--- src/data/card.ts
+++ src/data/card.ts
@@ -18,6 +18,98 @@ export default [
       img: require('../assets/image/card/1B.png'),
     },
   ],
+  [
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+  ],
+  [
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+  ],
+  [
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+  ],
+  [
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+    {
+      content: '人民英雄纪念碑',
+      img: require('../assets/image/card/1A.png'),
+    },
+    {
+      content: '2008 年北京奥运会',
+      img: require('../assets/image/card/1B.png'),
+    },
+  ],
 ] as CardData[][];
 
 // 单位 px
@@ -38,5 +130,41 @@ export const cardsSize: {
     imageHeight: 170,
     imageWidth: 212,
     fontSize: 24,
+  },
+  {
+    width: 234,
+    height: 296,
+    marginRight: 34,
+    marginBottom: 36,
+    imageHeight: 140,
+    imageWidth: 178,
+    fontSize: 22,
+  },
+  {
+    width: 193,
+    height: 246,
+    marginRight: 28,
+    marginBottom: 27,
+    imageHeight: 119,
+    imageWidth: 148,
+    fontSize: 18,
+  },
+  {
+    width: 166,
+    height: 209,
+    marginRight: 29,
+    marginBottom: 29,
+    imageHeight: 101,
+    imageWidth: 125,
+    fontSize: 16,
+  },
+  {
+    width: 149,
+    height: 190,
+    marginRight: 28,
+    marginBottom: 30,
+    imageHeight: 91,
+    imageWidth: 115,
+    fontSize: 14,
   },
 ]);

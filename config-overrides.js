9a8c6e2d3be33f8599d331dd31f431d97517ad36
skywalker <houzhenhong@hotmail.com>
2019-09-27 19:22:08 +0800

    chore(all): 不必要的文件

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const {
-  override,
-  addWebpackAlias,
-  // eslint-disable-next-line import/no-extraneous-dependencies
-} = require('customize-cra');
-
-module.exports = override(addWebpackAlias({}));

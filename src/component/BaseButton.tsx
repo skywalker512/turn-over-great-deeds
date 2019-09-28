23b33f202c8543858d47ad81060ad37758e0638d
skywalker <houzhenhong@hotmail.com>
2019-09-28 12:40:26 +0800

    fix(index): 解决图片被截断的问题

--- src/component/BaseButton.tsx
+++ src/component/BaseButton.tsx
@@ -3,7 +3,6 @@ import BaseOrangeButtonPng from '../assets/image/BaseOrangeButton.png';
 import BaseRedButtonPng from '../assets/image/BaseRedButton.png';
 
 const Button = styled.div`
-  background-size: cover;
   height: 62px;
   width: 167px;
   color: #f3efbf;
@@ -11,6 +10,8 @@ const Button = styled.div`
   line-height: 62px;
   text-align: center;
   font-family: LeZhen, sans-serif;
+  background-size: contain;
+  background-repeat: no-repeat;
 `;
 
 export const BaseOrangeButton = styled(Button)`

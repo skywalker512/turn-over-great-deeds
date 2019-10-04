61a7d1d13a2bad50cd98c567ea725431aba073ee
skywalker <houzhenhong@hotmail.com>
2019-10-04 17:29:18 +0800

    fix(base): 解决在渲染之前就去暂停播放

--- src/component/BaseAudio.tsx
+++ src/component/BaseAudio.tsx
@@ -1,20 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Mp3 from '../assets/1.mp3';
 
 const BaseAudio: React.FC = () => {
   const mp3Element = useRef<HTMLAudioElement>();
-  if (localStorage.getItem('musicStop')) {
-    // eslint-disable-next-line no-unused-expressions
-    mp3Element.current && mp3Element.current.pause();
-  }
-  window.addEventListener('musicPlay', () => {
-    // eslint-disable-next-line no-unused-expressions
-    mp3Element.current && mp3Element.current.play();
-  });
-  window.addEventListener('musicStop', () => {
-    // eslint-disable-next-line no-unused-expressions
-    mp3Element.current && mp3Element.current.pause();
-  });
+  useEffect(() => {
+    if (localStorage.getItem('musicStop')) {
+      // eslint-disable-next-line no-unused-expressions
+      mp3Element.current && mp3Element.current.pause();
+    }
+    window.addEventListener('musicPlay', () => {
+      // eslint-disable-next-line no-unused-expressions
+      mp3Element.current && mp3Element.current.play();
+    });
+    window.addEventListener('musicStop', () => {
+      // eslint-disable-next-line no-unused-expressions
+      mp3Element.current && mp3Element.current.pause();
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mp3Element.current]);
 
   // @ts-ignore
   // eslint-disable-next-line jsx-a11y/media-has-caption
